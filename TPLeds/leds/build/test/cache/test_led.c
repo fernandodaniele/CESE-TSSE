baebfb19e80a6c2d0542d0eb7bfcaafd92022ed0
Fernando Daniele <fernandodaniele1993@gmail.com>
2020-11-11 11:00:07 -0300

    Se crea proyecto

---
+++ TPLeds/leds/build/test/cache/test_led.c
@@ -0,0 +1,9 @@
+#include "build/temp/_test_led.c"
+#include "/var/lib/gems/2.5.0/gems/ceedling-0.30.0/vendor/unity/src/unity.h"
+
+
+void test_inicializacion(void){
+
+    UnityFail( (("Empezamos")), (UNITY_UINT)(4));
+
+}
---
+++ TPLeds/leds/build/test/preprocess/includes/test_led.c
@@ -0,0 +1,3 @@
+---
+- "/var/lib/gems/2.5.0/gems/ceedling-0.30.0/vendor/unity/src/unity.h"
+- build/temp/_test_led.c
